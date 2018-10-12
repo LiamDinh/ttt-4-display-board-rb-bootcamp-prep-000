1a57965382e60c476dc758698d58bc59974b208a
LOI PHAT DINH <liamphatdinh@gmail.com>
2018-10-12 19:42:37 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,3 +1,6 @@
 # Define display_board that accepts a board and prints
 # out the current state.
 def display_board
+  puts " #{board[0]}  | #{board[0]}   |  #{board[0]} "
+  puts "___________"
+  puts #{board[0]}
